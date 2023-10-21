a99bde4951a8e6a2989bead0dd072d4968ca6aaf
JABO Jean Marie <jabojeanmarie5@gmail.com>
2023-10-21 15:06:50 +0200

    Ad email confirmations

--- src/controllers/carOrderController.js
+++ src/controllers/carOrderController.js
@@ -1,4 +1,5 @@
 import CarOrder from "../models/carsOrder.js";
+import sendEmail from "../helpers/sendEmail.js";
 
 
 // create car order
@@ -13,6 +14,67 @@ export const orderCar = async (req, res) => {
         item: req.body.item,
         price: req.body.price, 
     });
+    const email = req.body.email;
+    const html = `<!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f0f0f0;
+      }
+      .container {
+        max-width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #ffffff;
+        border-radius: 5px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+      }
+      h1 {
+        color: #333;
+      }
+      p {
+        color: #555;
+      }
+      ul {
+        list-style: none;
+        padding: 0;
+      }
+      li {
+        color: #333;
+        margin-bottom: 10px;
+      }
+    </style>
+    </head>
+    <body>
+      <div class="container">
+        <h1>Order Confirmation</h1>
+        <p>Dear ${req.body.name},</p>
+        <p>Your car rental order with GAGA Cars has been received and is currently being processed. Here are the details of your request:</p>
+        <ul>
+          <li><strong>Order Number:</strong> [Your Order Number]</li>
+          <li><strong>Pickup Date and Time:</strong> [Date and Time]</li>
+          <li><strong>Return Date and Time:</strong> [Date and Time]</li>
+          <li><strong>Vehicle Type:</strong> [e.g., Economy, Compact, SUV, etc.]</li>
+          <li><strong>Pickup Location:</strong> [Location]</li>
+          <li><strong>Return Location:</strong> [Location]</li>
+          <li><strong>Total Cost:</strong> $[Amount]</li>
+        </ul>
+        <p>We'll review your request and ensure the availability of your selected vehicle. Please expect a confirmation and further instructions within [Specify the expected response time, e.g., 24 hours].</p>
+        <p>If you have any questions or need to make changes, please contact our customer service team at [Customer Service Phone Number] or [Customer Service Email Address].</p>
+        <p>Thank you for choosing GAGA Cars. We're committed to making your car rental experience a smooth and enjoyable one.</p>
+        <p>Best regards,<br>[Your Name]<br>[Your Title]<br>[Your Car Rental Company]</p>
+      </div>
+    </body>
+    </html>
+     `;
+    sendEmail(
+      '"GAGA Cars" <jaboinnovates@gmail.com>',
+      email,
+      "Order Confirmation: We've Received Your Car Rental Request",
+      html
+    );
     return res.status(201).json({ message: 'car order was sent successfully', order });
     } catch (error) {
       res.status(500).json({
--- src/controllers/partOrder.js
+++ src/controllers/partOrder.js
@@ -1,5 +1,6 @@
 import PartOrder from "../models/partOrder.js";
 import SparePart from "../models/sparePartModel.js";
+import sendEmail from "../helpers/sendEmail.js";
 // create a new part order
 
 export const createOrder = async (req, res) => {
@@ -12,6 +13,87 @@ export const createOrder = async (req, res) => {
         items: req.body.items,
         totalPrice: req.body.totalPrice, 
     });
+    const html = `<!DOCTYPE html>
+<html>
+<head>
+<style>
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+  }
+  .container {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #ffffff;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+  h1 {
+    color: #333;
+  }
+  p {
+    color: #555;
+  }
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  th, td {
+    border: 1px solid #ccc;
+    padding: 8px;
+    text-align: left;
+  }
+  th {
+    background-color: #f0f0f0;
+  }
+</style>
+</head>
+<body>
+  <div class="container">
+    <h1>Spare Parts Order Confirmation</h1>
+    <p>Dear ${req.body.name},</p>
+    <p>We are pleased to confirm that we have successfully received your spare parts order. Thank you for choosing [Your Company Name] for your automotive needs.</p>
+    <p>Here is a summary of your spare parts order:</p>
+    <ul>
+      <li><strong>Order Number:</strong> [Your Order Number]</li>
+      <li><strong>Order Date:</strong> [Date]</li>
+      <li><strong>Estimated Delivery Date:</strong> [Estimated Delivery Date]</li>
+      <li><strong>Shipping Address:</strong> [Shipping Address]</li>
+    </ul>
+    <p><strong>Order Details:</strong></p>
+    <table>
+      <tr>
+        <th>Part Number</th>
+        <th>Part Description</th>
+        <th>Quantity</th>
+        <th>Unit Price</th>
+        <th>Total Price</th>
+      </tr>
+      <tr>
+        <td>[Part Number]</td>
+        <td>[Part Description]</td>
+        <td>[Quantity]</td>
+        <td>$[Unit Price]</td>
+        <td>$[Total Price]</td>
+      </tr>
+    </table>
+    <p><strong>Total Amount:</strong> $[Total Amount]</p>
+    <p>Our team is diligently processing your order and will prepare it for shipment. Please note that the estimated delivery date is [Estimated Delivery Date]. You will receive a separate email with shipment tracking information once your order has been dispatched.</p>
+    <p>If you have any questions or require further assistance, please do not hesitate to contact our customer service team at [Customer Service Phone Number] or [Customer Service Email Address].</p>
+    <p>We appreciate your trust in [Your Company Name] for your spare parts needs. Our commitment is to provide you with high-quality parts and exceptional service.</p>
+    <p>Thank you for choosing us. We look forward to serving you.</p>
+    <p>Best regards,<br>[Your Name]<br>[Your Title]<br>[Your Company Name]<br>[Contact Information]</p>
+  </div>
+</body>
+</html>`;
+sendEmail(
+  '"GAGA Cars" <jaboinnovates@gmail.com>',
+  req.body.email,
+  "Order Confirmation: We've Received Your Spare Part Order",
+  html
+);
+
     return res.status(201).json({ message: 'Order created successfully', order });
     } catch (error) {
       res.status(500).json({
